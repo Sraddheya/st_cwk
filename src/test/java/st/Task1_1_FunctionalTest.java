44a61bb35a14dc9cb208bab6b83b3365484ab0d9
Sraddheya Gurung <ggsraddheya@gmail.com>
2022-03-15 17:14:25 +0000

    Added more bug tests

--- src/test/java/st/Task1_1_FunctionalTest.java
+++ src/test/java/st/Task1_1_FunctionalTest.java
@@ -33,11 +33,7 @@ public class Task1_1_FunctionalTest {
 		assertEquals(parser.getType("output"), Type.INTEGER);
 	}**/
 	
-	/**SPECIFICATION 1.3.2
-	 * Name AND shortcut of options should only contain digits, letters and underscores. 
-	 * Digits cannot appear as the first character. A runtime (IllegalArgumentException) exception is thrown otherwise.
-	 */
-
+	/**
 	@Test (expected=IllegalArgumentException.class)
 	public void optionNamingChars() {
 		//Names
@@ -48,22 +44,146 @@ public class Task1_1_FunctionalTest {
 		parser.addOption(new Option("output", Type.STRING), "*o");
 	}
 	
-	/**SPECIFICATION 1.3.3
-	 * Option names and shortcuts are case-sensitive.
-	 */
-	
-	@Test
+	@Test //[Bug #1 - Easy, 1PT]
 	public void optionNameCases() {
 		parser.addOption(new Option("output", Type.STRING));
 		assertFalse(parser.optionExists("OutPut"));
 		parser.addOption(new Option("OutPut", Type.STRING));
 		assertTrue(parser.optionExists("OutPut"));
 	}
-	@Test
+	
+	@Test //[Bug #1 - Easy, 1PT]
 	public void optionShortcutCases() {
 		parser.addOption(new Option("output", Type.STRING), "out");
 		assertFalse(parser.shortcutExists("Out"));
 		parser.setShortcut("output", "Out");
 		assertTrue(parser.shortcutExists("Out"));
 	}
+	
+	@Test //[Bug #1 - Easy, 1PT] //(expected=IllegalArgumentException.class)
+	public void optionShortcutSameName() {
+		parser.addOption(new Option("output", Type.STRING), "o");
+		parser.addOption(new Option("o", Type.INTEGER));
+		parser.parse("--o hello");
+		//assertEquals(parser.getString("output"), "hello");
+	}**/
+
+	/**
+	@Test //[Bug #2 - Easy, 1PT]
+	public void booleanValues() {
+		parser.addOption(new Option("output", Type.BOOLEAN), "o");
+		parser.parse("--output=0");
+		assertFalse(parser.getBoolean("output"));
+		parser.parse("-o=0");
+		assertFalse(parser.getBoolean("output"));
+		parser.parse("--output=false");
+		assertFalse(parser.getBoolean("output"));
+		parser.parse("-o=false");
+		assertFalse(parser.getBoolean("output"));
+		parser.parse("--output");
+		assertFalse(parser.getBoolean("output"));
+		parser.parse("-o");
+		assertFalse(parser.getBoolean("output"));
+		parser.parse("--output=anything");
+		assertTrue(parser.getBoolean("output"));
+		parser.parse("-o=anything");
+		assertTrue(parser.getBoolean("output"));
+	}
+	
+	@Test //[Bug #2 - Easy, 1PT]
+	public void booleanValues() {
+		parser.addOption(new Option("output", Type.BOOLEAN), "o");
+		parser.parse("--output=False");
+		assertFalse(parser.getBoolean("output"));
+		parser.parse("--output=false");
+		assertFalse(parser.getBoolean("output"));
+	}**/
+	
+	/**
+	@Test //[Bug #3 - Medium, 2PTS]
+	public void getIntegerTest() {
+		parser.addOption(new Option("input", Type.BOOLEAN));
+		parser.parse("--input=something");
+		assertEquals(parser.getInteger("input"), 1);
+		}**/
+	
+	/**
+	@Test //[Bug #4 - Medium, 2PTS]
+	public void shortcutLength() {
+		parser.addOption(new Option("input", Type.STRING), "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+		assertTrue(parser.optionOrShortcutExists("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"));
+	}**/
+	
+	/**
+	@Test //[Bug #5 - Medium, 2PTS]
+	public void negativeInteger() {
+		parser.addOption(new Option("input", Type.INTEGER));
+		parser.parse("--input=-1");
+		assertEquals(parser.getInteger("input"), -1);
+	}**/
+	
+	/**
+	@Test //[Bug #8 - Medium, 2PTS]
+	public void sameOptionDifferentShortcut() {
+		parser.addOption(new Option("input", Type.INTEGER), "i");
+		assertTrue(parser.optionOrShortcutExists("i"));
+		parser.addOption(new Option("input", Type.INTEGER), "in");
+		assertFalse(parser.optionOrShortcutExists("i"));
+		assertTrue(parser.optionOrShortcutExists("in"));
+	}**/
+	
+	/**
+	@Test //[Bug #10 - Easy, 1PT]
+	public void nullChar() {
+		parser.addOption(new Option("input", Type.CHARACTER));
+		parser.parse("--input=\0");
+		assertEquals(parser.getCharacter("input"), null);
+	}*/
+	
+	/**
+	@Test //[Bug #11 - Hard, 3PTS]
+	public void optionNaming() {
+		parser.addOption(new Option("input%", Type.STRING));
+		assertFalse(parser.optionExists("input%"));
+	}**/
+	
+	/**
+	@Test //[Bug #12 - Hard, 3PTS]
+	public void test() {
+		parser.addOption(new Option("input", Type.STRING), "i");
+		parser.addOption(new Option("option", Type.STRING), "input");
+		parser.parse("--input=something -input=something");
+		parser.replace("-input", "something", "nothing");
+		assertEquals(parser.getString("--input"), "nothing");
+	}**/
+	
+	/**
+	@Test //[Bug #15 - Medium, 2PTS]
+	public void largeInteger() {
+		parser.addOption(new Option("input", Type.STRING));
+		parser.parse("--input=9999999999");
+		assertEquals(parser.getInteger("input"), 9999999999L);
+		}**/
+	
+	/**
+	@Test //[Bug #17 - Hard, 3PTS]
+	public void optionLength() {
+		parser.addOption(new Option("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", Type.STRING));
+		assertTrue(parser.optionOrShortcutExists("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"));
+	}**/
+	
+	/**
+	@Test //[Bug #20 - Hard, 3PTS]
+	public void inputWithSpace() {
+		parser.addOption(new Option("input", Type.STRING));
+		parser.parse("--input=hello world");
+		assertEquals(parser.getString("input"), "hello world");
+	}**/
+	
+	@Test //[Bug #15 - Medium, 2PTS]
+	public void newLine() {
+		parser.addOption(new Option("input", Type.STRING));
+		parser.parse("--input=\\n");
+		assertEquals(parser.getInteger("input"), "\\n");
+		}
 }
